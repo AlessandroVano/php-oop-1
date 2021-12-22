c627f5b7dd5e16ef2cd33b47b5f1714eccab2576
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-12-22 15:14:39 +0100

    insert costruttore

--- movie.php
+++ movie.php
@@ -12,6 +12,16 @@ class Movie {
 
 
 
+// COSTRUTTORE   
+function __costruct($titolo, $autore, $trama, $anno ) {
+    echo "Istanza<br>";
+    $this->titolo = $titolo;
+    $this->autore = $autore;
+    $this->trama = $trama;
+    $this->anno = $anno;
+  }
+
+ 
 
 
 
