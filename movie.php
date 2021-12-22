39c6556246ebf1803efa4f190aa71829732d2727
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-12-22 15:14:01 +0100

    first commit

---
+++ movie.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Movie {
+    // PROPRIETA
+    public $titolo;
+    public $autore;
+    public $trama;
+    public $anno;
+    public $prezzoBiglietto;
+    public $scontoEta = 5;
+
+
+
+
+
+
+}
