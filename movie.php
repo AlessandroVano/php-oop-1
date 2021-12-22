52ce055698a9d5057c23f16670b1db42965180aa
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-12-22 16:21:46 +0100

    insert movie 1 + sconto

--- movie.php
+++ movie.php
@@ -8,17 +8,18 @@ class Movie {
     public $trama;
     public $anno;
     public $eta;
-    public $scontoEta = 5;
+    public $scontoeta = 5;
 
 
 
 // COSTRUTTORE   
-function __costruct($titolo, $autore, $trama, $anno ) {
+function __construct($titolo, $autore, $trama, $anno ) {
     echo "Istanza<br>";
     $this->titolo = $titolo;
     $this->autore = $autore;
     $this->trama = $trama;
     $this->anno = $anno;
+    
   }
 
   // METODI
@@ -37,3 +38,22 @@ function __costruct($titolo, $autore, $trama, $anno ) {
 }
 
 
+  // MOVIE 1
+
+
+  $movie1 = new Movie('Jurassic Park', 'Michael Crichton', 'Due paleontologi e un matematico sono tra le persone selezionate per partecipare a un giro organizzato a un parco a tema. Quando i dinosauri si animano e prendono il sopravvento, però, la visitasi trasforma presto in un incubo.', '1993');
+
+
+  $movie1->setAge(28);
+
+  var_dump($movie1);
+
+
+  // SCONTO  
+
+  $scontoFilm = $movie1->getScontoEta();
+
+  echo "Lo sconto per la tua età è $scontoFilm% ";
+
+
+  echo '<hr>';
