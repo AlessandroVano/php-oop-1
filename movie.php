ac0da4d66e8bf8f467072376f922449f63378619
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-12-22 17:29:53 +0100

    insert voto + film raccomandato + movie-3

--- movie.php
+++ movie.php
@@ -13,12 +13,14 @@ class Movie {
 
 
 // COSTRUTTORE   
-function __construct($titolo, $autore, $trama, $anno ) {
+function __construct($titolo, $autore, $trama, $anno, $voto ) {
     echo "Istanza<br>";
     $this->titolo = $titolo;
     $this->autore = $autore;
     $this->trama = $trama;
     $this->anno = $anno;
+    $this->voto = $voto;
+    $this->setFilmRaccomandato();
     
   }
 
@@ -35,13 +37,24 @@ function __construct($titolo, $autore, $trama, $anno ) {
 
        return $this->scontoeta;
   }
+
+  public function setFilmRaccomandato() {
+      if($this->voto > 6) {
+          $this->raccomandato = true;
+          echo 'ottimo film consigliato per tutti';
+      } else {
+        $this->raccomandato = false;
+        echo 'vi prego non andate al cinema';
+
+      }
+  }
 }
 
 
   // MOVIE 1
 
 
-  $movie1 = new Movie('Jurassic Park', 'Michael Crichton', 'Due paleontologi e un matematico sono tra le persone selezionate per partecipare a un giro organizzato a un parco a tema. Quando i dinosauri si animano e prendono il sopravvento, però, la visitasi trasforma presto in un incubo.', '1993');
+  $movie1 = new Movie('Jurassic Park', 'Michael Crichton', 'Due paleontologi e un matematico sono tra le persone selezionate per partecipare a un giro organizzato a un parco a tema. Quando i dinosauri si animano e prendono il sopravvento, però, la visitasi trasforma presto in un incubo.', '1993', 9);
 
 
   $movie1->setAge(28);
@@ -61,7 +74,7 @@ function __construct($titolo, $autore, $trama, $anno ) {
   // MOVIE 2
 
 
-  $movie2 = new Movie('Il Signore degli Anelli', 'Peter Jackson', 'Mentre Frodo e Sam, accompagnati da Gollum, proseguono il loro viaggio verso Monte Fato per distruggere l/anello, il resto della compagnia corre in soccorso di Rohan e Gondor, impegnati nella battaglia dei Campi del Pellenor..', '2004');
+  $movie2 = new Movie('Il Signore degli Anelli', 'Peter Jackson', 'Mentre Frodo e Sam, accompagnati da Gollum, proseguono il loro viaggio verso Monte Fato per distruggere l\anello, il resto della compagnia corre in soccorso di Rohan e Gondor, impegnati nella battaglia dei Campi del Pellenor..', '2004', 10);
 
 
   $movie2->setAge(82);
@@ -74,3 +87,34 @@ function __construct($titolo, $autore, $trama, $anno ) {
   $scontoFilm = $movie2->getScontoEta();
 
   echo "Lo sconto per la tua età è $scontoFilm% ";
+
+
+
+
+  echo '<hr>';
+
+
+   // MOVIE 3
+
+
+   $movie3 = new Movie('Me contro Te', 'Gianluca Leuzzi', 'Luì e Sofì sono una gioiosa e sorridente coppia di fidanzati, ma il temibile Signor S decide di dare loro dei problemi. Dopo averli catturati, il misterioso nemico crea due cloni della coppia, molto meno simpatici.', '2020', 1);
+
+
+   $movie3->setAge(12);
+ 
+   var_dump($movie3);
+ 
+ 
+   // SCONTO  
+ 
+   $scontoFilm = $movie3->getScontoEta();
+ 
+   echo "Lo sconto per la tua età è $scontoFilm% ";
+ 
+ 
+ 
+ 
+   
+
+
+
